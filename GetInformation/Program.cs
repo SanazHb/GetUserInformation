c6727cd9bc925c3b2b9ac953fd25b415ac1cda4a
sodamid <sodamid@DESKTOP-1RRSPKK>
2024-08-08 19:25:55 +0330

    Add project files.

---
+++ GetInformation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace GetInformation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName, lastName, age;
+            Console.WriteLine("Hello\nComplete your information.");
+            Console.WriteLine("Enter your first name please:");
+            firstName = Console.ReadLine();
+            while(string.IsNullOrEmpty(firstName) == true)
+            {
+                Console.WriteLine("Enter your first name please:");
+                firstName = Console.ReadLine();
+            }
+            Console.WriteLine("Enter your last name please:");
+            lastName = Console.ReadLine();
+            while(string.IsNullOrEmpty(lastName) == true)
+            {
+                Console.WriteLine("Enter your last name please:");
+                lastName = Console.ReadLine();
+            }
+            Console.WriteLine("Enter your age please:");
+            age = Console.ReadLine();
+            while(string.IsNullOrEmpty(age) == true)
+            {
+                Console.WriteLine("Enter your age please:");
+                age = Console.ReadLine();
+            }
+            Console.WriteLine("These are your information:\n"+firstName+" "+lastName+"\nAge: "+age);
+            Console.ReadLine();
+        }
+    }
+}
